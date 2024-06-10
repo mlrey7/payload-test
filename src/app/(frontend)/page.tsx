ee804ca4b836bf3a66fbdbc4f5a18790200b36a9
Matthew Rey <matthewlemuelr@gmail.com>
2024-06-10 14:27:42 +1000

    action test

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Page = () => {
   return (
     <div className="text-blue-500 text-lg font-bold">
-      Auto deployed with Github Actions
+      Auto deployed with Github Actions v2
       <Button>Click me</Button>
     </div>
   )
